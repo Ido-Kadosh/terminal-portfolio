f7b4812651a00ceccecc32099558b415e6732674
Ido Kadosh <1234kadosh@gmail.com>
2024-04-05 00:58:26 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Terminal from './cmps/Terminal';
+
+const App = () => {
+	return (
+		<div className="bg-main h-screen bg-no-repeat bg-cover bg-center">
+			<Terminal />
+		</div>
+	);
+};
+
+export default App;
