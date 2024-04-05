274a4a8a379a3928a6bb27d54bddb5fdb04b128e
Ido Kadosh <1234kadosh@gmail.com>
2024-04-05 03:48:37 +0300

    style: add responsive design for mobile

--- src/cmps/Terminal.tsx
+++ src/cmps/Terminal.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useLayoutEffect, useRef, useState } from 'react';
+import { useLayoutEffect, useRef, useState } from 'react';
 import Breadcrumbs from './Breadcrumbs';
 import Toolbar from './Toolbar';
 
@@ -29,11 +29,11 @@ const Terminal = () => {
 	};
 
 	return (
-		<main className="w-full 2xl:w-1/2 lg:w-2/3 max-w-7xl text-xl to-gray-300 flex flex-col h-1/2 text-gray-300 font-josefin">
+		<main className="w-full 2xl:w-1/2 lg:w-2/3 h-full overflow-auto lg:h-1/2 max-w-7xl text-xl to-gray-300 flex flex-col  text-gray-300 font-josefin">
 			<Toolbar />
 			<div
 				ref={scrollRef}
-				className="w-full bg-black bg-opacity-70 h-[55vh] overflow-auto hide-scrollbar rounded-b-md p-2 border-slate-800 border"
+				className="w-full bg-black bg-opacity-70 lg:h-[55vh] flex:1 overflow-y-auto overflow-x-hidden hide-scrollbar rounded-b-md p-2 border-slate-800 border"
 			>
 				<h1>Today is {getFormattedDate()}</h1>
 				{commands.map((command, idx) => (
